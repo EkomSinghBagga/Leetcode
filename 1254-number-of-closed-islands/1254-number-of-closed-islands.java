efb6c80dbe5d61a5a781d4cb919a9494af734998
EkomSinghBagga <89543622+EkomSinghBagga@users.noreply.github.com>
2022-05-26 21:11:37 +0530

    Time: 2 ms (89.79%), Space: 47.7 MB (14.27%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.java
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.java
@@ -1,25 +1,21 @@
 class Solution {
     public int closedIsland(int[][] grid) {
-        int count=0;
+        int c=0;
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]==0 && dfs(grid,i,j))
-                count++;
+                if(grid[i][j]==0&&isClosedIsland(grid,i,j))
+                    c++;
             }
         }
-        return count;
+        return c;
     }
-    public boolean dfs(int[][] grid,int i,int j){
-        if(i<0||i>grid.length-1||j<0||j>grid[0].length-1)
+    public boolean isClosedIsland(int[][] grid,int i,int j){
+        if(i<0||i>=grid.length||j<0||j>=grid[i].length)
             return false;
         if(grid[i][j]==1)
             return true;
         grid[i][j]=1;
-        boolean x1=dfs(grid,i+1,j);
-        boolean x2=dfs(grid,i-1,j);
-        boolean x3=dfs(grid,i,j+1);
-        boolean x4=dfs(grid,i,j-1);
+        return isClosedIsland(grid,i+1,j)&isClosedIsland(grid,i-1,j)&isClosedIsland(grid,i,j+1)&isClosedIsland(grid,i,j-1);
         
-        return x1 && x2 && x3 && x4;
     }
 }
