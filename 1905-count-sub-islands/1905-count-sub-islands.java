d6598d64357eed05c442f20ab593bb41fab1932c
EkomSinghBagga <89543622+EkomSinghBagga@users.noreply.github.com>
2022-05-28 23:31:37 +0530

    Time: 33 ms (48.27%), Space: 124.7 MB (64.33%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.java
+++ 1905-count-sub-islands/1905-count-sub-islands.java
@@ -1,26 +1,27 @@
 class Solution {
-    int flag=0;
     public int countSubIslands(int[][] grid1, int[][] grid2) {
-        int c=0;
+       int ans=0;
+        for(int i=0;i<grid1.length;i++){
+            for(int j=0;j<grid2[i].length;j++){
+                if(grid1[i][j]==0&&grid2[i][j]==1)
+                    dfs(grid1,grid2,i,j);
+            }
+        }
         for(int i=0;i<grid2.length;i++){
             for(int j=0;j<grid2[i].length;j++){
                 if(grid2[i][j]==1){
-                    // System.out.println(i+" "+j);
-                    flag=1;
+                    ans++;
                     dfs(grid1,grid2,i,j);
-                    c+=flag;
                 }
             }
         }
-        
-        return c;
+        return ans;
     }
-  
     public void dfs(int[][] grid1,int[][] grid2,int i,int j){
-        if(i<0||i>=grid1.length||j<0||j>=grid1[i].length||grid2[i][j]!=1)
+        if(i<0||i>=grid2.length||j<0||j>=grid2[0].length)
             return;
-    if(grid1[i][j]== 0)flag =0;
-
+        if(grid2[i][j]==0)
+            return;
         grid2[i][j]=0;
         dfs(grid1,grid2,i+1,j);
         dfs(grid1,grid2,i-1,j);
