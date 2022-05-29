297d489b76b734ede72580959231d4ac8fbd1a67
EkomSinghBagga <89543622+EkomSinghBagga@users.noreply.github.com>
2022-05-30 01:59:12 +0530

    Time: 10 ms (76.81%), Space: 45.9 MB (65.22%) - LeetHub

---
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxProduct(String[] words) {
+      int[] checker = new int[words.length];
+		int max = 0;
+		// populating the checker array with their respective numbers
+		for (int i = 0; i < checker.length; i++) {
+			int num = 0;
+			for (int j = 0; j < words[i].length(); j++) {
+				num |= 1 << (words[i].charAt(j) - 'a');
+			}
+			checker[i] = num;
+		}
+
+		for (int i = 0; i < words.length; i++) {
+			for (int j = i + 1; j < words.length; j++) {
+				if ((checker[i] & checker[j]) == 0) //checking if the two strings have common character
+					max = Math.max(max, words[i].length() * words[j].length());
+			}
+		}
+		return max;  
+    }
+}
