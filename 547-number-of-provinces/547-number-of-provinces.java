33ea0a71d2795a750a2d94198fc6b08c1d9fcd5f
EkomSinghBagga <89543622+EkomSinghBagga@users.noreply.github.com>
2022-06-08 13:34:45 +0530

    Time: 6 ms (22.23%), Space: 54.2 MB (32.56%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.java
+++ 547-number-of-provinces/547-number-of-provinces.java
@@ -1,32 +1,36 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
+        int n=isConnected.length;
+        List<List<Integer>> adj=new ArrayList<>();
+        createGraph(adj,n,isConnected);
         int ans=0;
-        List<List<Integer>>adj=new ArrayList<>();
-        for(int i=0;i<isConnected.length;i++){
-            adj.add(new ArrayList<>());
+        boolean[] visited=new boolean[n];
+        for(int i=0;i<n;i++){
+            if(!visited[i])
+               { dfs(adj,visited,i);
+                ans++;}
+            
         }
-        for(int i=0;i<isConnected.length;i++){
-            for(int j=0;j<isConnected[0].length;j++){
-                if(isConnected[i][j]==1)
-                    adj.get(i).add(j);
-            }
-        }
-        boolean[] visited=new boolean[isConnected.length];
-        Arrays.fill(visited,false);
-        for(int i=0;i<isConnected.length;i++){
-            if(!visited[i]){
-                dfs(adj,i,visited);
-                ans++;
-            }
-        }
-        return ans;
+        return ans; 
     }
-    public void dfs(List<List<Integer>> adj,int i,boolean[] visited){
-        if(visited[i]==true)
-            return;
-        visited[i]=true;
-        for(int j:adj.get(i)){
-            dfs(adj,j,visited);
+    public void createGraph(List<List<Integer>> adj,int n,int[][] isConnected){
+        for(int i=0;i<n;i++)
+            adj.add(new ArrayList<>());
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j]==1)
+                {
+                    adj.get(i).add(j);
+                    adj.get(j).add(i);
+                }
+            }
         }
+        
+    }
+    public void dfs(List<List<Integer>> adj,boolean[] visited,int i){
+        if(visited[i]) return;
+        visited[i]=true;
+        for(int j:adj.get(i))
+            dfs(adj,visited,j);
     }
 }
