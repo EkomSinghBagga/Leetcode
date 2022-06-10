f71aeab36601498d6e3055c4e7a14c051f4c2a4f
EkomSinghBagga <89543622+EkomSinghBagga@users.noreply.github.com>
2022-06-10 15:39:23 +0530

    Time: 2 ms (61.99%), Space: 43.9 MB (38.09%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.java
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -1,34 +1,18 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-       List<String> ans=new ArrayList<>();
-        backtrack(n,ans,"");
+        List<String> ans=new ArrayList<>();
+        backtrack(n,ans,"",0,0);
         return ans;
     }
-    public void backtrack(int n,List<String> ans,String cur){
-        if(cur.length()==2*n)
-        {
-            if(isValid(cur))
-                ans.add(cur);
+    public void backtrack(int n,List<String> ans,String cur,int open,int close){
+        if(cur.length()==2*n){
+            ans.add(cur);
             return;
         }
-        backtrack(n,ans,cur+'(');
-        backtrack(n,ans,cur+')');
-    }
-    public boolean isValid(String ans){
-        Stack<Character> st=new Stack<>();
-        for(char ch:ans.toCharArray()){
-            if(ch==')')
-            {
-                if(st.isEmpty())
-                    return false;
-                else
-                    st.pop();
-            }
-            else
-                st.push('(');
-            
+        if(open<n){
+            backtrack(n,ans,cur+'(',open+1,close);
         }
-        return st.isEmpty();
+        if(close<open)
+            backtrack(n,ans,cur+')',open,close+1);
     }
-    
 }
