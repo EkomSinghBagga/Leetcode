4823077d463a7823078f02266d6b714d8c5e55a2
EkomSinghBagga <89543622+EkomSinghBagga@users.noreply.github.com>
2022-06-11 18:20:38 +0530

    Time: 40 ms (22.60%), Space: 42.4 MB (99.95%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.java
+++ 47-permutations-ii/47-permutations-ii.java
@@ -1,27 +1,28 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
-       List<List<Integer>> res = new ArrayList<List<Integer>>();
-        if(nums==null || nums.length==0) return res;
-        boolean[] used = new boolean[nums.length];
-        List<Integer> list = new ArrayList<Integer>();
-        Arrays.sort(nums);
-        dfs(nums, used, list, res);
+        Set<List<Integer>> ans=new HashSet<>();
+        List<List<Integer>> res=new ArrayList<>();
+        boolean[] visited=new boolean[nums.length];
+        backtrack(nums,ans,visited,new ArrayList<>());
+        for(List<Integer> l:ans){
+            res.add(l);
+        }
         return res;
     }
-
-    public void dfs(int[] nums, boolean[] used, List<Integer> list, List<List<Integer>> res){
-        if(list.size()==nums.length){
-            res.add(new ArrayList<Integer>(list));
-            return;
-        }
-        for(int i=0;i<nums.length;i++){
-            if(used[i]) continue;
-            if(i>0 &&nums[i-1]==nums[i] && !used[i-1]) continue;
-            used[i]=true;
-            list.add(nums[i]);
-            dfs(nums,used,list,res);
-            used[i]=false;
-            list.remove(list.size()-1);
+    public void backtrack(int[] nums,Set<List<Integer>> ans,boolean[] visited,List<Integer> cur){
+       if(cur.size()==nums.length) 
+       {
+           ans.add(new ArrayList<>(cur));
+           return;
+       }
+        for(int j=0;j<nums.length;j++){
+            if(!visited[j]){
+                visited[j]=true;
+                cur.add(nums[j]);
+                backtrack(nums,ans,visited,cur);
+                cur.remove(cur.size()-1);
+                visited[j]=false;
+            }
         }
     }
 }
