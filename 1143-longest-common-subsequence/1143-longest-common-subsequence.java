cafa3b62171bc2088118260f35f61259b53ad7d0
EkomSinghBagga <89543622+EkomSinghBagga@users.noreply.github.com>
2022-06-13 15:02:31 +0530

    Time: 27 ms (24.10%), Space: 46.7 MB (26.64%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,28 +1,20 @@
 class Solution {
-    int[][] memo;
     public int longestCommonSubsequence(String text1, String text2) {
-        memo=new int[text1.length()][text2.length()];
-        for(int i=0;i<text1.length();i++){
-            for(int j=0;j<text2.length();j++){
-                memo[i][j]=-1;
+        int m=text1.length(),n=text2.length();
+        int[][] dp=new int[m+1][n+1];
+        for(int i=0;i<=m;i++){
+            dp[i][0]=0;
+        }
+        for(int i=0;i<=n;i++)
+            dp[0][i]=0;
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(text2.charAt(j-1)==text1.charAt(i-1))
+                    dp[i][j]=1+dp[i-1][j-1];
+                else
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
             }
         }
-        return lcs(text1,text2,text1.length()-1,text2.length()-1);
-    }
-    
-    public int lcs(String a,String b,int m,int n){
-        if(m<0||n<0)
-            return 0;
-        if(memo[m][n]!=-1)
-            return  memo[m][n];
-        if(a.charAt(m)==b.charAt(n))
-           { memo[m][n]=1+lcs(a,b,m-1,n-1);
-            return memo[m][n];}
-        else
-        {
-            memo[m][n]= Math.max(lcs(a,b,m-1,n),lcs(a,b,m,n-1));
-            return memo[m][n];
-            
-    }
+        return dp[m][n];
     }
 }
