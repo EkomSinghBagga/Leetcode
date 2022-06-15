fac8f826859915ba18fb53515827aee970def8f9
EkomSinghBagga <89543622+EkomSinghBagga@users.noreply.github.com>
2022-06-15 16:39:15 +0530

    Time: 8 ms (61.52%), Space: 44.9 MB (52.29%) - LeetHub

--- 72-edit-distance/72-edit-distance.java
+++ 72-edit-distance/72-edit-distance.java
@@ -1,21 +1,19 @@
 class Solution {
-    Integer[][] memo;
     public int minDistance(String word1, String word2) {
-        memo=new Integer[word1.length()+1][word2.length()+1];
-        return solve(word1,word2,word1.length(),word2.length());
-    }
-    public int solve(String word1,String word2,int n1,int n2){
-        if(n1==0||n2==0)
-            return Math.max(n1,n2);
-        if(memo[n1][n2]!=null)
-            return memo[n1][n2];
-        if(word1.charAt(n1-1)==word2.charAt(n2-1))
-        {
-            return memo[n1][n2]=solve(word1,word2,n1-1,n2-1);
+        int n1=word1.length(),n2=word2.length();
+       int[][] dp=new int[n1+1][n2+1];
+        for(int i=0;i<=n1;i++){
+            for(int j=0;j<=n2;j++){
+                if(i==0)
+                    dp[i][j]=j;
+                else if(j==0)
+                    dp[i][j]=i;
+                else if(word1.charAt(i-1)==word2.charAt(j-1))
+                    dp[i][j]=dp[i-1][j-1];
+                else
+                    dp[i][j]=1+Math.min(dp[i][j-1],Math.min(dp[i-1][j-1],dp[i-1][j]));
+            }
         }
-        else
-        {
-            return memo[n1][n2]=1+Math.min(solve(word1,word2,n1-1,n2),Math.min(solve(word1,word2,n1,n2-1),solve(word1,word2,n1-1,n2-1)));
-        }
+        return dp[n1][n2];
     }
 }
