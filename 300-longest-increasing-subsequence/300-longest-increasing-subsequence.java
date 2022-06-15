c2bd592fef4e4c819210e54652ef393fda3d52f2
EkomSinghBagga <89543622+EkomSinghBagga@users.noreply.github.com>
2022-06-15 21:31:42 +0530

    Time: 8 ms (76.66%), Space: 44.1 MB (60.58%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,18 +1,31 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int n=nums.length;
-        int[] dp=new int[n];
-        dp[0]=1;
-        for(int i=1;i<n;i++){
-            dp[i]=1;
-            for(int j=0;j<i;j++){
-                if(nums[j]<nums[i])
-                    dp[i]=Math.max(dp[i],dp[j]+1);
-            }
+        ArrayList<Integer> tail=new ArrayList<>();
+        tail.add(nums[0]);
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]>tail.get(tail.size()-1))
+                tail.add(nums[i]);
+            else{
+                int x=ceil(tail,nums[i]);
+                tail.set(x,nums[i]);
+                    
+                }
         }
-        int max=0;
-        for(int i:dp)
-            max=Math.max(i,max);
-        return max;
+        return tail.size();
+            
+    }
+    public int ceil(ArrayList<Integer> a,int x){
+        int st=0,end=a.size()-1,mid,ans=0;
+        while(st<=end){
+            mid=st+(end-st)/2;
+            if(a.get(mid)>=x)
+            {
+                ans=mid;
+                end=mid-1;
+            }
+            else
+                st=mid+1;
+        }
+        return ans;
     }
 }
