3d74944fe72d6107253ad010cc2f48a3c7b50a74
EkomSinghBagga <89543622+EkomSinghBagga@users.noreply.github.com>
2022-06-16 20:33:25 +0530

    Time: 9 ms (91.46%), Space: 42.9 MB (90.94%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.java
@@ -1,26 +1,18 @@
 class Solution {
     Integer[][] memo;
     public int minDistance(String word1, String word2) {
-        int n1=word1.length(),n2=word2.length();
-        memo=new Integer[n1+1][n2+1];
-        int l=lcs(word1,word2,n1,n2);
-        // System.out.println(l);
-        return n1+n2-l-l;
+        memo=new Integer[word1.length()+1][word2.length()+1];
+        return solve(word1,word2,word1.length(),word2.length());
     }
-    public int lcs(String word1,String word2,int n1,int n2){
+    public int solve(String s1,String s2,int n1,int n2){
+        if(n1==0||n2==0)
+            return Math.max(n1,n2);
         if(memo[n1][n2]!=null)
             return memo[n1][n2];
-        if(n1==0||n2==0)
-            return memo[n1][n2]=0;
-        if(word1.charAt(n1-1)==word2.charAt(n2-1))
-        {
-            memo[n1][n2]=1+lcs(word1,word2,n1-1,n2-1);
-            return memo[n1][n2];
+        if(s1.charAt(n1-1)==s2.charAt(n2-1)){
+            return memo[n1][n2]=solve(s1,s2,n1-1,n2-1);
         }
-        else
-        {
-            memo[n1][n2]=Math.max(lcs(word1,word2,n1-1,n2),lcs(word1,word2,n1,n2-1));
-            return memo[n1][n2];
-        }
+        return memo[n1][n2]=1+Math.min(solve(s1,s2,n1-1,n2),solve(s1,s2,n1,n2-1));
+        
     }
 }
