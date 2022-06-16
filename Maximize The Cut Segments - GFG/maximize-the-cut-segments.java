cdd705236fd3d88a68883afb7519e699009494f5
EkomSinghBagga <89543622+EkomSinghBagga@users.noreply.github.com>
2022-06-16 17:32:32 +0530

    Added solution - LeetHub

---
+++ Maximize The Cut Segments - GFG/maximize-the-cut-segments.java
@@ -0,0 +1,70 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Driver
+{
+    public static void main(String args[])throws IOException
+    {
+        //reading input using BufferedReader class
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        
+        //reading total testcases
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            //reading length of line segment
+            int n = Integer.parseInt(read.readLine().trim());
+            
+            
+            //reading 3 segment lengths
+            String str[] = read.readLine().trim().split(" ");
+            
+            int x = Integer.parseInt(str[0]);
+            int y = Integer.parseInt(str[1]);
+            int z = Integer.parseInt(str[2]);
+            
+            
+            //calling method maximizeCuts() of class Solution()
+            //and printinting the result
+            System.out.println(new Solution().maximizeCuts(n, x, y, z));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution
+{
+    //Function to find the maximum number of cuts.
+    Integer[] memo;
+    public int maximizeCuts(int n, int x, int y, int z)
+    {
+       //Your code here
+       //Your code here
+        memo=new Integer[n+1];
+       int t=solve(n,x,y,z);
+       if(t>=0)
+       return t;
+       return 0;
+    }
+    public int solve(int n,int x,int y,int z){
+    
+     if(n<0)
+        return -10000;
+    if(memo[n]!=null)
+        return memo[n];
+        if(n==0)
+            return 0;
+        return memo[n]=1+Math.max(solve(n-x,x,y,z),Math.max(solve(n-y,x,y,z),solve(n-z,x,y,z)));
+ 
+      
+    }
+}
