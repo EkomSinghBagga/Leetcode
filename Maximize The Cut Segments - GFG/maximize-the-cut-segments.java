5c5ae80fc0279f9fb91125d0994130f45cdb52d9
EkomSinghBagga <89543622+EkomSinghBagga@users.noreply.github.com>
2022-06-20 16:26:25 +0530

    Added solution - LeetHub

--- Maximize The Cut Segments - GFG/maximize-the-cut-segments.java
+++ Maximize The Cut Segments - GFG/maximize-the-cut-segments.java
@@ -44,19 +44,25 @@ class Driver
 class Solution
 {
     //Function to find the maximum number of cuts.
-    public int maximizeCuts(int n, int a, int b, int c)
+    static Integer[] memo;
+    public int maximizeCuts(int n, int x, int y, int z)
     {
        //Your code here
-       int[] dp=new int[n+1];
-       Arrays.fill(dp,-1);
-       dp[0]=0;
-       for(int i=1;i<=n;i++){
-           if(i-a>=0) dp[i]=Math.max(dp[i],dp[i-a]);
-           if(i-b>=0) dp[i]=Math.max(dp[i],dp[i-b]);
-           if(i-c>=0) dp[i]=Math.max(dp[i],dp[i-c]);
-           if(dp[i]!=-1)
-           dp[i]++;
-       }
-       return dp[n]==-1?0:dp[n];
+         memo=new Integer[n+1];
+        int a=solve(n,x,y,z);
+        if(a<0)
+        return 0;
+        
+        return a;
+    }
+    static int solve(int n,int x,int y,int z){
+        if(n==0)
+            return 0;
+        if(n<0)
+            return -100000;
+        if(memo[n]!=null)
+        return memo[n];
+        return memo[n]=1+Math.max(solve(n-x,x,y,z),Math.max(solve(n-y,x,y,z),solve(n-z,x,y,z)));
+    
     }
 }
