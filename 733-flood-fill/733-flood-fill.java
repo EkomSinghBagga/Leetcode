3a8012f1047e75b1038e134a4d2da0e64a8a855a
EkomSinghBagga <89543622+EkomSinghBagga@users.noreply.github.com>
2022-06-29 18:34:26 +0530

    Time: 1 ms (89.89%), Space: 48.2 MB (39.20%) - LeetHub

--- 733-flood-fill/733-flood-fill.java
+++ 733-flood-fill/733-flood-fill.java
@@ -1,19 +1,18 @@
 class Solution {
-    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
-        int color=image[sr][sc];
-        if(color!=newColor){
-            dfs(image,sr,sc,newColor,color);
-        }
-    
-    return image;
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int oldColor=image[sr][sc];
+        if(oldColor==color)
+            return image;
+        dfs(image,sr,sc,color,oldColor);
+        return image;
     }
-    public void dfs(int[][] image,int i,int j,int newColor,int color){
-        if(i<0||j<0||i>=image.length||j>=image[0].length||image[i][j]!=color)
+    int[][] dir={{1,0},{0,1},{-1,0},{0,-1}};
+    public void dfs(int[][] image,int row,int col,int color,int old){
+        if(row<0||row>=image.length||col<0||col>=image[0].length||image[row][col]!=old)
             return;
-        image[i][j]=newColor;
-        dfs(image,i+1,j,newColor,color);
-        dfs(image,i,j+1,newColor,color);
-        dfs(image,i-1,j,newColor,color);
-        dfs(image,i,j-1,newColor,color);
+        image[row][col]=color;
+        for(int[] d:dir){
+            dfs(image,row+d[0],col+d[1],color,old);
+        }
     }
 }
