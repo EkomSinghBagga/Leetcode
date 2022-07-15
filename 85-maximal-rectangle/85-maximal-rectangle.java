8c590622b7f1ac642b31ad63a6639fd1409a25c2
EkomSinghBagga <89543622+EkomSinghBagga@users.noreply.github.com>
2022-07-16 00:40:12 +0530

    Time: 77 ms (16.66%), Space: 54.6 MB (54.92%) - LeetHub

---
+++ 85-maximal-rectangle/85-maximal-rectangle.java
@@ -0,0 +1,58 @@
+class Solution {
+    public int maximalRectangle(char[][] matrix) {
+        int n=matrix.length,m=matrix[0].length;
+        int[][] mat=new int[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                mat[i][j]=matrix[i][j]-'0';
+            }
+        }
+        int max=solve(mat[0]);
+        for(int i=1;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==1)
+                    mat[i][j]=mat[i-1][j]+1;
+                // System.out.print(mat[i][j]+" ");
+            }
+            // System.out.println();
+            max=Math.max(max,solve(mat[i]));
+        }
+    return max;
+        
+    }
+    public int solve(int[] heights){
+               int n=heights.length;
+       Stack<Integer> st1=new Stack<>();
+        int[] ps=new int[n];
+        for(int i=0;i<n;i++){
+            while(!st1.isEmpty()&&heights[st1.peek()]>=heights[i]){
+                st1.pop();
+            }
+            if(st1.isEmpty())
+                ps[i]=-1;
+            else
+                ps[i]=st1.peek();
+            st1.push(i);
+        }
+        Stack<Integer> st2=new Stack<>();
+        int[] ns=new int[n];
+        for(int i=n-1;i>=0;i--){
+            while(!st2.isEmpty()&&heights[st2.peek()]>=heights[i]){
+                st2.pop();
+            }
+            if(st2.isEmpty())
+                ns[i]=n;
+            else
+                ns[i]=st2.peek();
+            st2.push(i);
+        }
+        int max=0;
+        for(int i=0;i<n;i++){
+            int cur=heights[i];
+            cur+=(i-ps[i]-1)*heights[i];
+            cur+=(ns[i]-i-1)*heights[i];
+            max=Math.max(max,cur);
+        }
+        return max;
+    }
+}
